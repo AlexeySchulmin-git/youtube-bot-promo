1999f42d71a5f6b7a1ccd47f6c494cf83101d644
AlexeySchulmin-git <alexeyschulmin@gmail.com>
2026-05-27 21:47:54 +0300

    вымыв

---
+++ config.js
@@ -0,0 +1,7 @@
+// Render can generate this file at deploy time, or you can edit manually.
+// Use only public values (anon key), never service_role.
+window.__APP_CONFIG__ = {
+  SUPABASE_URL: "https://owiydstyqpqvnbevvxsb.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_aL9hMDfqujmavC0mUEJdyQ_5iOE9rGO",
+  SUPABASE_SCHEMA: "public",
+};
